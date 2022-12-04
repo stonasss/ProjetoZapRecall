e987a5b78b3e2c376eb3493272ea8a2ed88dd3d3
Luis Bernardo <102603174+stonasss@users.noreply.github.com>
2022-12-04 19:59:12 -0300

    adição de mais um estado + botões

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,7 +15,7 @@ export default function Card({
     questionsAnswered,
     setQuestionsAnswered
 }) {
-
+    console.log(questionsClicked)
     return (
         <>
             <ClosedQuestion display={questionsClicked.includes(card) ? true : false}>
@@ -25,12 +25,27 @@ export default function Card({
                 </img>
             </ClosedQuestion>
 
-            <OpenQuestion display={questionsClicked.includes(card) ? false : true}>
+            <OpenQuestion display={!questionsClicked.includes(card) || questionsTurned.includes(card) ? true : false}>
                 <p>{card.question}</p>
                 <img src={turn}
                     onClick={() => { setQuestionsTurned([...questionsTurned, card]) }}>
                 </img>
             </OpenQuestion>
+
+            <Answer display={questionsTurned.includes(card) ? false : true}>
+                <p>{card.answer}</p>
+                <span>
+                    <Button color="#FF3030">
+                        Não lembrei
+                    </Button>
+                    <Button color="#FF922E">
+                        Quase não lembrei
+                    </Button>
+                    <Button color="#2FBE34">
+                        Zap!
+                    </Button>
+                </span>
+            </Answer>
         </>
     )
 }
@@ -43,7 +58,7 @@ const ClosedQuestion = styled.div`
     padding: 15px;
     box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
     border-radius: 5px;
-    display: flex;
+    display: ${props => props.display ? 'none' : 'flex'};
     align-items: center;
     justify-content: space-between;
 
@@ -72,7 +87,7 @@ const OpenQuestion = styled.div`
     line-height: 22px;
     color: #333333;
     position: relative;
-    display: flex;
+    display: ${props => props.display ? 'none' : 'flex'};
     flex-direction: column;
     justify-content: space-between;
 
@@ -81,4 +96,46 @@ const OpenQuestion = styled.div`
         bottom: 10px;
         right: 10px;
     }
+`
+const Answer = styled.div`
+    width: 300px;
+    margin: 12px;
+    padding: 15px;
+    min-height: 100px;
+    background: #FFFFD5;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #333333;
+    position: relative;
+    display: ${props => props.display ? 'none' : 'flex'};
+    flex-direction: column;
+    justify-content: space-between;
+
+    span {
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
+const Button = styled.button`
+    width: 90px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #FFFFFF;
+    background: ${props => props.color};
+    border-radius: 5px;
+    border: 1px solid;
+    padding:5px;
 `
